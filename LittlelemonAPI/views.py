fa338bf621e66774f2770316091547ab1c5fcae9
bing tian <bingtian@bings-MacBook-Pro.local>
2023-09-14 16:15:42 -0600

    convert to DRF

--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics 
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer 
+
+class SingleMenuItemsView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer 
 # Create your views here.
